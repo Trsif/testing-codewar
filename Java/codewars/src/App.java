d76d49e77ccd6910eec28766f416a39bdc29baf0
Tristan Favard <pepefava1@gmail.com>
2020-11-01 20:56:17 -0500

    problem for  the day

---
+++ Java/codewars/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int size;
+        int sum=0;
+        System.out.println("size of array");
+        size = sc.nextInt();
+        int[] a = new int [size];
+        System.out.println("enter array items");
+        for(int i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int num : a ){
+            sum += (num*num); 
+        }
+        System.out.println(sum);
+    }
+}
+
